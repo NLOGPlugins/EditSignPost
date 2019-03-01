ad7ec83e32dc3f2e2818362a54668e5535ac450c
nnnlog <sorisem4106@naver.com>
2019-03-01 14:30:06 +0900

    License missing

--- src/nlog/editsign/EditSignPostCommand.php
+++ src/nlog/editsign/EditSignPostCommand.php
@@ -1,13 +1,22 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Home
- * Date: 2019-03-01
- * Time: 오후 2:15
+ * Copyright (C) 2017-2019  NLOG(엔로그)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 namespace nlog\editsign;
-
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
